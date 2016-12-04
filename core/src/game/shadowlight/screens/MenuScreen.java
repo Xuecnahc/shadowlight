7d3f29ce6496cc2154b3f648133956c126b3234b
Bertrand Hieronymus <zulrage.hieronymus@gmail.com>
2016-12-04 14:46:35 +0900

    - Add font, menuSkin and skin - Add button in the menu - Remove old button and title

--- core/src/game/shadowlight/screens/MenuScreen.java
+++ core/src/game/shadowlight/screens/MenuScreen.java
@@ -2,28 +2,23 @@ package game.shadowlight.screens;
 
 import com.badlogic.gdx.Application.ApplicationType;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Pixmap;
-import com.badlogic.gdx.graphics.Pixmap.Format;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.World;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 import game.shadowlight.ShadowLightGame;
 import game.shadowlight.entities.levelObjects.Box;
+import game.shadowlight.screens.ui.MenuGroup;
 
 public class MenuScreen extends ShadowLightScreen {
 
-	private BitmapFont font;
 	private Skin skin;
 	private Stage stage;
 
@@ -42,11 +37,6 @@ public class MenuScreen extends ShadowLightScreen {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-		batch.begin();
-		//TODO: Change with localization
-        font.draw(batch, "Hello World", 200, 200);
-        batch.end();
-
         // Draw the actors of the scene (mostly buttons)
         stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
 		stage.draw();
@@ -56,41 +46,31 @@ public class MenuScreen extends ShadowLightScreen {
 	public void show() {
 		world = new World(new Vector2(0, -9.81f), true);
 		batch = new SpriteBatch();
-	    font = new BitmapFont();
-	    font.setColor(Color.RED);
+	   
 	    tmpBodies.add((new Box(world, 1f, 1f, 1f, 1f)).getBody());
 
 	    stage = new Stage();
-	    Gdx.input.setInputProcessor(stage);
+		InputMultiplexer multiplexer = new InputMultiplexer();
+		multiplexer.addProcessor(new InputAdapter() {
 
-		// TODO: See what the hell is going on with this textButtonStyle. Shouldn't be hard...
-		skin = new Skin();
-		// Generate a 1x1 white texture and store it in the skin named "white".
-		Pixmap pixmap = new Pixmap(100, 50, Format.RGBA8888);
-		pixmap.setColor(Color.GREEN);
-		pixmap.fill();
-		skin.add("white", new Texture(pixmap));
-		// Store the default libgdx font under the name "default".
-		BitmapFont bfont=new BitmapFont();
-		skin.add("default",bfont);
-		TextButtonStyle textButtonStyle = new TextButtonStyle();
-		textButtonStyle.up = skin.newDrawable("white", Color.DARK_GRAY);
-		textButtonStyle.down = skin.newDrawable("white", Color.DARK_GRAY);
-		textButtonStyle.checked = skin.newDrawable("white", Color.BLUE);
-		textButtonStyle.over = skin.newDrawable("white", Color.LIGHT_GRAY);
-
-		textButtonStyle.font = skin.getFont("default");
-		final TextButton textButton=new TextButton("PLAY", textButtonStyle);
-		textButton.setPosition(1, 1);
-		stage.addActor(textButton);
-
-		// Listener
-		textButton.addListener(new ClickListener() {
-			public void clicked (InputEvent event, float blop, float blip) {
-				textButton.setText("Starting new game");
-				game.setScreen(new PlayScreen());
+			@Override
+			public boolean keyDown(int keycode) {
+				switch (keycode) {
+				case Keys.BACK:
+				case Keys.ESCAPE:
+					Gdx.app.exit();
+					break;
+				}
+				return false;
 			}
+
 		});
+
+		Gdx.input.setInputProcessor(multiplexer);
+		stage = new Stage();
+		multiplexer.addProcessor(stage);
+		skin = new Skin(Gdx.files.internal("ui/menuSkin.json"), new TextureAtlas("ui/atlas.pack"));
+		stage.addActor(new MenuGroup(skin, game));
 
 		if(Gdx.app.getType() == ApplicationType.Desktop) {
 			Gdx.graphics.setDisplayMode((int) (Gdx.graphics.getHeight() / 1.5f), Gdx.graphics.getHeight(), false);
