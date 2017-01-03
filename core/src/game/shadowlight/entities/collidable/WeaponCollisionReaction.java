3adda387c7cb4e6d556e369adb55da0deaac4db8
Xuecnahc <XuecnahcHiero@gmail.com>
2017-01-03 10:22:11 +0100

    Remove unsed code

--- core/src/game/shadowlight/entities/collidable/WeaponCollisionReaction.java
+++ core/src/game/shadowlight/entities/collidable/WeaponCollisionReaction.java
@@ -1,6 +1,5 @@
 package game.shadowlight.entities.collidable;
 
-import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.Contact;
 import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.Manifold;
@@ -10,7 +9,7 @@ import game.shadowlight.utils.EnumUserDataId;
 
 public class WeaponCollisionReaction implements ICollisionReaction {
   private boolean isAlly;
-  
+
   public void setIsAlly(boolean isAlly) {
     this.isAlly = isAlly;
   }
@@ -19,27 +18,17 @@ public class WeaponCollisionReaction implements ICollisionReaction {
   public boolean canCollide(Fixture fixtureA, Fixture fixtureB) {
     GenericUserData userDataA = (GenericUserData) fixtureA.getBody().getUserData();
     GenericUserData userDataB = (GenericUserData) fixtureB.getBody().getUserData();
-    
+
     if (this.isAlly) {
       // no friendly fire
       return !userDataA.getId().equals(EnumUserDataId.PLAYER) && !userDataB.getId().equals(EnumUserDataId.PLAYER);
     }
-    
+
     return true;
   }
 
   @Override
   public void onCollide(Contact contact, Manifold oldManifold) {
-    GenericUserData userDataA = (GenericUserData) contact.getFixtureA().getBody().getUserData();
-    Body playerBody = null;
-    if (userDataA != null && userDataA.getId().equals(EnumUserDataId.PLAYER)) {
-      playerBody = contact.getFixtureA().getBody();
-    } else {
-      playerBody = contact.getFixtureB().getBody();
-    }
-    if (contact.getWorldManifold().getPoints()[0].y <= playerBody.getPosition().y) {
-      ((GenericUserData) playerBody.getUserData()).setNbJump(0);
-    }
   }
 
 }
